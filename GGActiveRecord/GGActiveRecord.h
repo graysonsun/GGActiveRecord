1c61db36d18558544b7169f95a0441e3139620bb
Grayson <267357374@qq.com>
2014-05-27 22:31:57 +0800

    create project

---
+++ GGActiveRecord/GGActiveRecord.h
@@ -0,0 +1,13 @@
+//
+//  GGActiveRecord.h
+//  GGActiveRecord
+//
+//  Created by graysonsun-nb.local on 5/27/14.
+//  Copyright (c) 2014 GeekDreaming. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GGActiveRecord : NSObject
+
+@end
---
+++ GGActiveRecord/GGActiveRecord.m
@@ -0,0 +1,13 @@
+//
+//  GGActiveRecord.m
+//  GGActiveRecord
+//
+//  Created by graysonsun-nb.local on 5/27/14.
+//  Copyright (c) 2014 GeekDreaming. All rights reserved.
+//
+
+#import "GGActiveRecord.h"
+
+@implementation GGActiveRecord
+
+@end
